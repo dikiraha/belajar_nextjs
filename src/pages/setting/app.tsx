7fc8fea7c1f57d70d16f574a648a27e00ba74ad1
dikiraha <kiiinugraha123@gmail.com>
2024-03-12 08:57:08 +0700

    belajar layouting

---
+++ src/pages/setting/app.tsx
@@ -0,0 +1,9 @@
+const AppSeetingPage = () => {
+  return (
+    <div>
+      <h1>App Page</h1>
+    </div>
+  );
+};
+
+export default AppSeetingPage;
---
+++ src/pages/setting/user.tsx
@@ -0,0 +1,9 @@
+const UserSeetingPage = () => {
+  return (
+    <div>
+      <h1>User Page</h1>
+    </div>
+  );
+};
+
+export default UserSeetingPage;
